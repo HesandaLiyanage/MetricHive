682c2a02d290f0d511e41f677025b8f120a8ca47
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-08 17:55:09 +0530

    init

---
+++ src/main/java/com/hess/metrichive/MetricHiveApplication.java
@@ -0,0 +1,13 @@
+package com.hess.metrichive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricHiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricHiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hess/metrichive/MetricHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hess.metrichive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricHiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
