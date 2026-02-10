7fbff9d62e76f16e2f118c51613b3b929577f3c0
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-10 18:48:17 +0530

    MetricHive - 2026-02-10 18:48:17

--- src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
+++ src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
@@ -1,11 +1,13 @@
 package com.hess.metrichive.Service;
 
+import com.hess.metrichive.Model.Metric;
 import com.hess.metrichive.Repository.MetricRepository;
 import com.hess.metrichive.dto.MetricDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -18,6 +20,15 @@ public class MetricIngestionService {
 
     public void ingestMetrics(Long tenantId, List<MetricDTO> metricDTOs) {
         log.info("Ingesting {} metrics for tenant {}" , metricDTOs.size());
+
+        List<Metric> metrics = metricDTOs.stream()
+                .map(dto -> convertToEntity(tenantId,dto))
+                .collect(Collectors.toList());
+    }
+
+    private Metric convertToEntity(Long tenantId, MetricDTO dto) {
+        Metric metric = new Metric();
+        Metric.setTenantId(tenantId);
     }
 
 
