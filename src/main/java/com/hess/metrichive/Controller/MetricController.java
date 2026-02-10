19be84907bf677094b424ab1df4a85b1eb3a3c84
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-11 04:14:25 +0530

    MetricHive - 2026-02-11 04:14:25

--- src/main/java/com/hess/metrichive/Controller/MetricController.java
+++ src/main/java/com/hess/metrichive/Controller/MetricController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -39,6 +36,11 @@ public class MetricController {
                 "status" , "success",
                 "ingested" , request.getMetrics().size()
         ));
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<?>  health() {
+        return ResponseEntity.ok(Map.of("status" , "healthy"));
     }
 
 }
