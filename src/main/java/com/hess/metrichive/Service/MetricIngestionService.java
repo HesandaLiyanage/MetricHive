d9571978ae657e21769fce3bda112e4a07145f8b
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-10 18:38:13 +0530

    MetricHive - 2026-02-10 18:38:13

--- src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
+++ src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
@@ -1,19 +1,22 @@
 package com.hess.metrichive.Service;
 
 import com.hess.metrichive.Repository.MetricRepository;
+import com.hess.metrichive.dto.MetricDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MetricIngestionService {
 
     private final MetricRepository metricRepository;
-    public void MetricRepository(MetricRepository metricRepository) {
-        this.
-
+    public MetricIngestionService(MetricRepository metricRepository) {
+        this.metricRepository = metricRepository;
     }
 
-
-
+    public void ingestMetrics(Long tenantId, List<MetricDTO> metricDTOs) {
+        log.info
+    }
 
 
 }
