545deab7802b631db7c0581007d6d31c7e825a4f
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-10 19:03:23 +0530

    MetricHive - 2026-02-10 19:03:23

--- src/main/java/com/hess/metrichive/Controller/MetricController.java
+++ src/main/java/com/hess/metrichive/Controller/MetricController.java
@@ -1,8 +1,13 @@
 package com.hess.metrichive.Controller;
 
 
+import com.hess.metrichive.Service.MetricIngestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class MetricController {
-    
+    public final MetricIngestionService metricIngestionService;
+
+    @PostMapping("/ingest")
+    public ResponseEntity<?> ingestMetrics(@Valid @RequestBody)
 
 }
