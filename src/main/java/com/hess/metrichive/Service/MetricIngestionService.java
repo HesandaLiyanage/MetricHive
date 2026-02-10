1da3c1f74e7102e3b03317c4cfa63657f1921dfc
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-10 18:28:11 +0530

    MetricHive - 2026-02-10 18:28:11

--- src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
+++ src/main/java/com/hess/metrichive/Service/MetricIngestionService.java
@@ -1,14 +1,19 @@
 package com.hess.metrichive.Service;
 
 import com.hess.metrichive.Repository.MetricRepository;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class MetricIngestionService {
 
     private final MetricRepository metricRepository;
+    public void MetricRepository(MetricRepository metricRepository) {
+        this.
+
+    }
+
+
+
+
 
 }
