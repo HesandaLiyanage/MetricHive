74dae54c11f604db17032e79b337fca0da15f6cd
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-11 08:13:01 +0530

    MetricHive - 2026-02-11 08:13:01

--- src/main/java/com/hess/metrichive/Config/DevelopmentDataLoader.java
+++ src/main/java/com/hess/metrichive/Config/DevelopmentDataLoader.java
@@ -1,17 +1,28 @@
 package com.hess.metrichive.Config;
 
-
+import com.hess.metrichive.Model.Metric;
+import com.hess.metrichive.Model.Tenant;
+import com.hess.metrichive.Repository.MetricRepository;
+import com.hess.metrichive.Repository.TenantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
-@Profile("dev")
+@Profile("dev")  // Only runs in dev profile
 @RequiredArgsConstructor
 @Slf4j
 public class DevelopmentDataLoader implements CommandLineRunner {
-    
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
