9c175bf139e8906d8189d1a52ad0f1e5c9e0f23a
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-14 11:36:20 +0530

    MetricHive - 2026-02-14 11:36:20

--- src/main/java/com/hess/metrichive/Config/RedisConfig.java
+++ src/main/java/com/hess/metrichive/Config/RedisConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
@@ -12,6 +15,8 @@ public class RedisConfig {
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.)
     }
 }
