ef7314f095f5253b5768e4d912174b2a95d1d853
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-16 13:28:54 +0530

    MetricHive - 2026-02-16 13:28:54

--- src/main/java/com/hess/metrichive/Config/Bucket4jConfig.java
+++ src/main/java/com/hess/metrichive/Config/Bucket4jConfig.java
@@ -1,14 +1,20 @@
 package com.hess.metrichive.Config;
 
 
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class Bucket4jConfig {
@@ -24,6 +30,22 @@ public class Bucket4jConfig {
         return redisClient.connect(
                 RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE)
         );
+    }
+
+    @Bean
+    public ProxyManager<String> bucketProxyManager(
+            StatefulRedisConnection<String, byte[]> redisConnection) {
+
+        return LettuceBasedProxyManager
+                .builderFor(redisConnection)
+                .withExpirationAfterWriteStrategy(
+                        ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
+                                Duration.ofMinutes(1)
+                        )
+                )
+                .build();
+        // ProxyManager is the thing that creates/manages buckets in Redis
+        // one bucket per API key, all stored in Redis
     }
 
 }
