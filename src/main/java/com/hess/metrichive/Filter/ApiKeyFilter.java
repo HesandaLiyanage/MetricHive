6ff229859fe90e663df8a1d5514d8576a4ae950e
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-18 19:59:39 +0530

    MetricHive - 2026-02-18 19:59:39

---
+++ src/main/java/com/hess/metrichive/Filter/ApiKeyFilter.java
@@ -0,0 +1,9 @@
+package com.hess.metrichive.Filter;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+}
