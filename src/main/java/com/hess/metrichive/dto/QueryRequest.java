b05553fe730613119c5ae61b7c3dc57f250064ed
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-24 11:28:42 +0530

    MetricHive - 2026-02-24 11:28:42

--- src/main/java/com/hess/metrichive/dto/QueryRequest.java
+++ src/main/java/com/hess/metrichive/dto/QueryRequest.java
@@ -1,8 +1,10 @@
 package com.hess.metrichive.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -10,14 +12,17 @@ import java.util.Optional;
 @Data
 public class QueryRequest {
 
-    @NotBlank(message = "API key is required")
-    private String apiKey;
-
+    @NotBlank(message = "Metric Name is required")
     public String metric_name;
+
+    @NotBlank(message = "Aggregation is required")
     public String aggregation;
 
+    @NotNull(message = "Start time is required")
     public LocalDateTime start_time;
-    public LocalDateTime end_time;
+    
+    @NotNull(message = "End time is required")
+    private Instant endTime;
 
     public Optional<String> interval;
     public String[] groupBy;
