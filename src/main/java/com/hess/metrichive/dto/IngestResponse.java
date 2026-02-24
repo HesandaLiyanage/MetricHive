d55e738719cf2bbb7c46275ae935450a3fe89d2e
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-02-24 11:18:36 +0530

    MetricHive - 2026-02-24 11:18:36

--- src/main/java/com/hess/metrichive/dto/IngestResponse.java
+++ src/main/java/com/hess/metrichive/dto/IngestResponse.java
@@ -1,4 +1,19 @@
 package com.hess.metrichive.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IngestResponse {
+    public String status;
+    public String metricsReceived;
+    public long processingTimeMs;
+    public String requestId;
+
 }
