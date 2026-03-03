3d91b8b516a7a694c322f28303e1be196e4c919e
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-03-03 13:25:27 +0530

    MetricHive - 2026-03-03 13:25:27

---
+++ src/main/java/com/hess/metrichive/Service/MetricQueryService.java
@@ -0,0 +1,13 @@
+package com.hess.metrichive.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
+@Slf4j
+public class MetricQueryService {
+}
