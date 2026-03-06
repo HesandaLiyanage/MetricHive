9afca196186cd6cc252dde5cee7ceded4f66ea6d
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-03-06 10:31:40 +0530

    MetricHive - 2026-03-06 10:31:40

--- src/main/java/com/hess/metrichive/dto/IngestResponse.java
+++ src/main/java/com/hess/metrichive/dto/IngestResponse.java
@@ -1,10 +1,8 @@
 package com.hess.metrichive.dto;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import tools.jackson.databind.PropertyNamingStrategies;
 import tools.jackson.databind.annotation.JsonNaming;
 
