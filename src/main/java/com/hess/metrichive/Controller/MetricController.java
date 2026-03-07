217f2a0d9845b92904c007e8d2718eea5f97281c
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-03-07 10:34:06 +0530

    MetricHive - 2026-03-07 10:34:06

--- src/main/java/com/hess/metrichive/Controller/MetricController.java
+++ src/main/java/com/hess/metrichive/Controller/MetricController.java
@@ -4,48 +4,41 @@ import com.hess.metrichive.dto.IngestRequest;
 import com.hess.metrichive.dto.IngestResponse;
 import com.hess.metrichive.Security.TenantContext;
 import com.hess.metrichive.Service.MetricIngestionService;
-import com.hess.metrichive.dto.QueryRequest;
-import com.hess.metrichive.dto.QueryResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/metrics")
+@RequiredArgsConstructor
 @Slf4j
 public class MetricController {
 
-    @Autowired
-    private MetricIngestionService metricIngestionService;
+    private final MetricIngestionService metricIngestionService;
 
     @PostMapping("/ingest")
-    public ResponseEntity<IngestResponse> ingest(@RequestBody @Valid IngestRequest request) {
-
-        // Start timer
+    public ResponseEntity<IngestResponse> ingest(@Valid @RequestBody IngestRequest request) {
         long startTime = System.currentTimeMillis();
 
-        // Get current tenant (from API key authentication)
+        // 1. Get current tenant from the security context
         Long tenantId = TenantContext.getTenantId();
 
-        // Log
-        log.info("Ingesting {} metrics for tenant {}",
-                request.getMetrics().size(), tenantId);
+        log.info("Received {} metrics for ingestion from tenant {}", request.getMetrics().size(), tenantId);
 
-        // Call service to do the actual work
-        metricIngestionService.ingestBatch(request.getMetrics());
+        // 2. Pass to service layer
+        metricIngestionService.ingestBatch(tenantId, request.getMetrics());
 
-        // Calculate processing time
+        // 3. Formulate Response
         long processingTime = System.currentTimeMillis() - startTime;
 
-        // Return response
-        return ResponseEntity.ok(IngestResponse.builder()
+        IngestResponse response = IngestResponse.builder()
                 .status("success")
-                .metricsReceived(String.valueOf(request.getMetrics().size()))
+                .metricsReceived(request.getMetrics().size())
                 .processingTimeMs(processingTime)
-                .build());
+                .build();
+
+        return ResponseEntity.ok(response);
     }
-
-
 }
