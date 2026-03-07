ce6f357fc02529b4e2d376f548bdc6d1025b810c
Hesanda Liyanage <130324291+HesandaLiyanage@users.noreply.github.com>
2026-03-07 09:38:59 +0530

    MetricHive - 2026-03-07 09:38:59

--- src/main/java/com/hess/metrichive/Infrastructure/RateLimit/RateLimitInterceptor.java
+++ src/main/java/com/hess/metrichive/Infrastructure/RateLimit/RateLimitInterceptor.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
-public class RateLimitInterceptor implements HandlerInterceptor {
+public class hRateLimitInterceptor implements HandlerInterceptor {
 
     private final RateLimitService rateLimitService;
 
